4f9a4fd18a37851bc7e9418d638ee4104ff345a0
Prashant Sharma <ps.arraha@gmail.com>
2022-06-19 14:31:25 +0200

    Update TargetSumIndex.java

--- twoweekschallange/easy/TargetSumIndex.java
+++ twoweekschallange/easy/TargetSumIndex.java
@@ -1,6 +1,8 @@
 package twoweekschallange.easy;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -27,6 +29,23 @@ public class TargetSumIndex {
 
         return (pos);
     }
+    
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                ans[1] = i;
+                ans[0] = map.get(target - nums[i]);
+                return ans;
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return ans;
+    }    
 
     public int[] twoSum1(int[] numbers, int target) {
         int[] res = new int[2];
