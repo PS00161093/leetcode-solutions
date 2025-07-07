88a1eb88abe6ac8fbb41512175a6a55644aa6a6c
Prashant Sharma <ps.arraha@gmail.com>
2025-07-07 19:12:09 +0200

    Jump Game
    Jump Game

---
+++ top-150/arrays/MaxJump.java
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/jump-game/?envType=study-plan-v2&envId=top-interview-150
+
+class Solution {
+    public boolean canJump(int[] nums) {
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > max) {
+                return false;
+            }
+            max = Math.max(nums[i] + i, max);
+        }
+        return true;
+    }
+}
