b6772a17a48e3b91bc0b12151190b521cc2e1d34
Prashant Sharma <ps.arraha@gmail.com>
2025-08-02 06:55:48 +0200

    move-zeroes

---
+++ top-150/arrays/easy/MoveZeros.java
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/move-zeroes/description/
+
+class MoveZeros {
+    public void moveZeroes(int[] nums) {
+        int left = 0;
+        int right = 0;
+        while (right < nums.length) {
+            if (nums[right] != 0) {
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                left++;
+            }
+            right++;
+        }
+    }
+}
