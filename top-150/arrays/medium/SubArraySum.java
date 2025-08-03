3aa64df8f5ffb2a2b51f588970f35b014c09ed46
Prashant Sharma <ps.arraha@gmail.com>
2025-08-03 20:56:01 +0200

    subarray-sum-equals-k

---
+++ top-150/arrays/medium/SubArraySum.java
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/subarray-sum-equals-k/?envType=company&envId=facebook&favoriteSlug=facebook-six-months
+
+class SubArraySum {
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int sum = 0;
+        int result = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum = sum + nums[i];
+
+            if (map.containsKey(sum - k)) {
+                result = result + map.get(sum - k);
+            }
+
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return result;
+    }
+}
