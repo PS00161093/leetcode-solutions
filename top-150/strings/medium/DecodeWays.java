5ae2a09afe44c5702886f7b806e8b76ea921360d
Prashant Sharma <ps.arraha@gmail.com>
2025-08-06 22:08:37 +0200

    decode-ways

---
+++ top-150/strings/medium/DecodeWays.java
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/decode-ways/?envType=company&envId=facebook&favoriteSlug=facebook-six-months
+// Time: O(n), Space: O(n)
+
+class DecodeWays {
+    public int numDecodings(String s) {
+        int[] dp = new int[s.length() + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+
+        for (int i = 2; i <= s.length(); i++) {
+            int d1 = Integer.valueOf(s.substring(i - 1, i));
+            int d2 = Integer.valueOf(s.substring(i - 2, i));
+            if (d1 >= 1) {
+                dp[i] = dp[i] + dp[i - 1];
+            }
+
+            if (d2 >= 10 && d2 <= 26) {
+                dp[i] = dp[i] + dp[i - 2];
+            }
+        }
+
+        return dp[s.length()];
+    }
+}
