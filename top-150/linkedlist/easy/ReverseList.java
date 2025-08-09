364dff5a414b8a66d8bb99c4923370f7f8b77cf8
Prashant Sharma <ps.arraha@gmail.com>
2025-08-09 11:05:02 +0200

    reverse-linked-list

---
+++ top-150/linkedlist/easy/ReverseList.java
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/reverse-linked-list/
+
+class ReverseList {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+
+        return prev;
+    }
+}
