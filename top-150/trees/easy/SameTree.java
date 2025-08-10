fd28396a23e9e8c95d3509062137d0e615a1acd3
Prashant Sharma <ps.arraha@gmail.com>
2025-08-10 11:24:18 +0200

    same-tree

---
+++ top-150/trees/easy/SameTree.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/same-tree/
+class SameTree {
+    public boolean isSameTree(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return true;
+        }
+        if (t1 == null || t2 == null) {
+            return false;
+        }
+        if (t1.val != t2.val) {
+            return false;
+        }
+
+        return isSameTree(t1.left, t2.left) && isSameTree(t1.right, t2.right);
+    }
+}
