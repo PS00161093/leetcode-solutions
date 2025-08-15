9ce3abdf93f63c633f7fee46a44c1fac3ad9a6a6
Prashant Sharma <ps.arraha@gmail.com>
2025-08-15 07:09:52 +0200

    contains-duplicate

---
+++ top-150/arrays/easy/ContainsDuplicate.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/contains-duplicate/description/
+
+class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) {
+            if (set.contains(n)) {
+                return true;
+            } else {
+                set.add(n);
+            }
+        }
+
+        return false;
+    }
+}
